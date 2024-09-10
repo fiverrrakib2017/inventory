bb2806113a63de61d369db3f44bb502c3adbd2b9
Rakib Mahmud <rakibas375@gmail.com>
2024-09-10 23:48:57 +0600

    customer crud completed 10/09/2024

--- app/Http/Controllers/Backend/Customer/CustomerController.php
+++ app/Http/Controllers/Backend/Customer/CustomerController.php
@@ -76,27 +76,10 @@ class CustomerController extends Controller
       
     public function store(Request $request)
     {
-        // Validate the form data
         $rules=[
             'fullname' => 'required|string',
-            'email_address' => 'required|email',
-            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'phone_number' => 'required|string',
-            'e_contract' => 'required|string',
-            'city' => 'required|string',
-            'state' => 'required|string',
+            'phone_number' => 'required|string|unique:customers,phone_number',
             'address' => 'required|string',
-            'date_of_birth' => 'required|date',
-            'gender' => 'required|in:1,0',
-            'marital_status' => 'required|in:1,2,3',
-            'verification_status' => 'required|in:1,2',
-            'verification_info' => 'nullable|string',
-            'opening_balance' => 'nullable|numeric',
-            'bank_name' => 'required|string',
-            'bank_account_name' => 'required|string',
-            'bank_acc_no' => 'required|string',
-            'bank_routing_no' => 'required|numeric',
-            'bank_payment_status' => 'required|in:1,2',
         ];
         $validator = Validator::make($request->all(), $rules);
 
@@ -107,41 +90,16 @@ class CustomerController extends Controller
             ], 422);
         }
 
-        // Handle file upload
-        if ($request->hasFile('profile_image')) {
-            $image = $request->file('profile_image');
-            $imageName = time() . '.' . $image->getClientOriginalExtension();
-            $image->move(public_path('Backend/uploads/photos'), $imageName);
-        } else {
-            $imageName = '';
-        }
 
         // Create a new Customer
         $object = new Customer();
         $object->fullname = $request->fullname;
-        $object->profile_image = $imageName;
-        $object->email_address = $request->email_address;
         $object->phone_number = $request->phone_number;
-        $object->emergency_contract = $request->e_contract;
-        $object->city = $request->city;
-        $object->state = $request->state;
         $object->address = $request->address;
-        $object->dob = $request->date_of_birth;
-        $object->gender = $request->gender;
-        $object->marital_status = $request->marital_status;
-        $object->verification_status = $request->verification_status;
-        $object->verification_info = $request->verification_info;
-        $object->opening_balance = $request->opening_balance;
-        $object->bank_name = $request->bank_name;
-        $object->bank_acc_name = $request->bank_account_name;
-        $object->bank_acc_no = $request->bank_acc_no;
-        $object->bank_routing_no = $request->bank_routing_no;
-        $object->bank_payment_status = $request->bank_payment_status;
-        // Save to the database table
+        /*Save to the database table*/ 
         $object->save();
 
-        // Redirect to the index page or show success message
-        //return redirect()->route('admin.customer.index')->with('success', 'Customer added successfully');
+       
         return response()->json([
             'success' => true,
             'message' => 'Customer added successfully!'
@@ -156,16 +114,6 @@ class CustomerController extends Controller
         if (empty($object)) {
             return response()->json(['error' => 'Customer not found.'], 404);
         }
-
-        /* Image Find And Delete it From Local Machine */
-        if (!empty($object->profile_image)) {
-            $imagePath = public_path('Backend/uploads/photos/' . $object->profile_image);
-
-            if (file_exists($imagePath)) {
-                unlink($imagePath);
-            }
-        }
-
         /* Delete it From Database Table */
         $object->delete();
 
@@ -191,24 +139,8 @@ class CustomerController extends Controller
         /*Validate the form data*/ 
         $rules=[
             'fullname' => 'required|string',
-            'email_address' => 'required|email',
-            'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif|max:2048',
-            'phone_number' => 'nullable|string',
-            'e_contract' => 'nullable|string',
-            'city' => 'nullable|string',
-            'state' => 'nullable|string',
-            'address' => 'nullable|string',
-            'date_of_birth' => 'nullable|date',
-            'gender' => 'nullable|in:1,0',
-            'marital_status' => 'nullable|in:1,2,3',
-            'verification_status' => 'nullable|in:1,2',
-            'verification_info' => 'nullable|string',
-            'opening_balance' => 'nullable|numeric',
-            'bank_name' => 'nullable|string',
-            'bank_account_name' => 'nullable|string',
-            'bank_acc_no' => 'nullable|string',
-            'bank_routing_no' => 'nullable|numeric',
-            'bank_payment_status' => 'nullable|in:1,2',
+            'phone_number' => 'required|string|unique:customers,phone_number,' . $id,
+            'address' => 'required|string',
         ];
         $validator = Validator::make($request->all(), $rules);
         if ($validator->fails()) {
@@ -222,41 +154,8 @@ class CustomerController extends Controller
 
         $object = Customer::findOrFail($id);
         $object->fullname = $request->fullname;
-        
-        // Handle profile image update
-        if ($request->hasFile('profile_image')) {
-
-            // Delete previous image
-            if (!empty($object->profile_image)) {
-                $imagePath = public_path('Backend/uploads/photos/' . $object->profile_image);
-                if (file_exists($imagePath)) {
-                    unlink($imagePath);
-                }
-            }
-
-            $imageName = time() . '.' . $request->file('profile_image')->getClientOriginalExtension();
-            $request->file('profile_image')->move(public_path('Backend/uploads/photos'), $imageName);
-
-            $object->profile_image = $imageName;
-        }
-
-        $object->email_address = $request->email_address;
         $object->phone_number = $request->phone_number;
-        $object->emergency_contract = $request->e_contract;
-        $object->city = $request->city;
-        $object->state = $request->state;
         $object->address = $request->address;
-        $object->dob = $request->date_of_birth;
-        $object->gender = $request->gender;
-        $object->marital_status = $request->marital_status;
-        $object->verification_status = $request->verification_status;
-        $object->verification_info = $request->verification_info;
-        $object->opening_balance = $request->opening_balance;
-        $object->bank_name = $request->bank_name;
-        $object->bank_acc_name = $request->bank_account_name;
-        $object->bank_acc_no = $request->bank_acc_no;
-        $object->bank_routing_no = $request->bank_routing_no;
-        $object->bank_payment_status = $request->bank_payment_status;
         $object->update();
 
         return response()->json([
